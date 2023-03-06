85632174ab0bdc628d41584520315b73d9c48e51
Akash A <akash.a.dckap@gmail.com>
2023-03-06 11:08:51 +0530

    fawes

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const answer1 = document.querySelector(".month-interest")
 const answer2 = document.querySelector(".total-paying")
 // console.log(answer2)
 const options = document.querySelector(".options")
-// console.log(options)
+// console.log(options)Number
 
-
+ 
 
 
 
@@ -34,25 +34,29 @@ calculate.addEventListener("click", (e) => {
     e.preventDefault()
     if (principalamt.value !== "" && interest.value !== "" && timelimit.value !== "" && options.value == "years") {
         // if(options.value == "years"){
-            let yearConventor = (Number(timelimit.value)*12)
+            let yearConventor = (timelimit.value)*12
         // console.log(yearConventor);
-        let rateOfInt = Number(interest.value / 1200)
+        let rateOfInt = interest.value / 1200
         // console.log(rateOfInt)
 
-        let Numarator = (Number(principalamt.value) * rateOfInt) * (Number(1 + rateOfInt)) ** (Number(yearConventor))
+        let Numarator = (principalamt.value) * rateOfInt * (1 + rateOfInt) ** (yearConventor)
         // console.log(Numarator)
 
-        let Demnaminator = (Number(1 + rateOfInt) ** (Number(yearConventor)) - 1)
+        let Demnaminator = (1 + rateOfInt) ** (yearConventor) - 1
         // console.log(Demnaminator)
 
-        let result =Math.floor(Numarator / Demnaminator)
+        let result =Number(Numarator / Demnaminator)
+        answer.innerText = Math.round(result).toLocaleString()
         // console.log(result)
+        
+        let output = result * yearConventor - principalamt.value
+    //    console.log()
+        answer1.innerText = Math.round(output).toLocaleString()
 
-        let output = result * (Number(yearConventor))
+        let output2 =   Number(principalamt.value)+Number(output)
+        answer2.innerText = Math.round(output2).toLocaleString()
 
-        let output2 = output - (Number(principalamt.value))
 
-        answer.innerText = result
         // const createli = document.createElement("li")
         // createli.innerText = result
         // createli.style.listStyle = "none"
@@ -64,13 +68,12 @@ calculate.addEventListener("click", (e) => {
         // createli1.style.listStyle = "none"
         // answer1.append(createli1)
         // interest.value = ""
-        answer1.innerText = output2
+     
         // const createli2 = document.createElement("li")
         // createli2.innerText = output
         // createli2.style.listStyle = "none"
         // answer2.append(createli2)
         // timelimit.value = ""
-        answer2.innerText = output
         // }
        
       
@@ -85,10 +88,11 @@ calculate.addEventListener("click", (e) => {
         let Demnaminator = (Number(1 + rateOfInt) ** (Number(timelimit.value)) - 1)
         // console.log(Demnaminator)
 
-        let result = Math.floor(Numarator / Demnaminator)
+        let result = Math.round(Numarator / Demnaminator)
         // console.log(result)
-
-        let output = result * (Number(timelimit.value))
+        let result1 = Number(Numarator/Demnaminator)
+        
+        let output = Math.round(result1 * (Number(timelimit.value)))
 
         let output2 = output - (Number(principalamt.value))
 
